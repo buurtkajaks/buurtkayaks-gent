1c21f301fb3f9d06b80d3ebbcf7142028cfb6505
Frederik Cruyt <31966290+fcruyt@users.noreply.github.com>
2022-07-22 13:22:39 +0200

    mapped kayaks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 const App = () => {
+
+  const kayaks = [
+    {
+      id: 1,
+      title: "Kajak 1",
+      places: 2
+    },
+    {
+      id: 2,
+      title: "Kajak 2",
+      places: 2
+    },
+    {
+      id: 3,
+      title: "Kajak 3",
+      places: 2
+    },
+    {
+      id: 4,
+      title: "Kajak 4",
+      places: 1
+    },
+    {
+      id: 5,
+      title: "Kajak 5",
+      places: 1
+    }
+  ];
+
   return (
     <div className="kayaks-container">
-      <div className="kayak-container">
-        {/*<img/>*/}
-          <div className="kayak-body-container">
-            <h2>Kayak 1</h2>
-            <p>2 personen</p>
-            <p>Reserveer nu</p>
-          </div>      
-      </div>
+      {kayaks.map(({title, places}) => (
+        <div className="kayak-container">
+          <div className="background-image" />
+            <div className="kayak-body-container">
+              <h2>{title}</h2>
+              <p>{places}-persoons</p>
+              <p>Reserveer nu</p>
+            </div>      
+        </div>
+      ))}
     </div>
   );
 }
