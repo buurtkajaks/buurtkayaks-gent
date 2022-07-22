5387d5bc27ecf50cf0a7ed18de1f64fafffe062a
Frederik Cruyt <31966290+fcruyt@users.noreply.github.com>
2022-07-22 11:40:02 +0200

    scaffold react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="kayaks-container">
+      <div className="kayak-container">
+        {/*<img/>*/}
+          <div className="kayak-body-container">
+            <h2>Kayak 1</h2>
+            <p>2 personen</p>
+            <p>Reserveer nu</p>
+          </div>      
+      </div>
     </div>
   );
 }
