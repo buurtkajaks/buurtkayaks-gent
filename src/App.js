b5d58a9685fe22c4992ec31573ac3d977dcdd461
Frederik Cruyt <31966290+fcruyt@users.noreply.github.com>
2022-07-23 13:49:46 +0200

    navbar styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import { Routes, Route, Outlet } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
 
-const Navigation = () => {
+const Kayaks = () => {
   return(
-    <div>
-      <div>
-        <h1> I am the navigation bar</h1>
-      </div>
-      <Outlet/>
-    </div>
-  )
-}
+    <h1>Kayak page</h1>
+  );
+} 
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation/>}>
         <Route index element={<Home />}/>
-        <Route path="home" element={<Home />}/>
+        <Route path="kayaks" element={<Kayaks />}/>
       </Route>
     </Routes>
   );
