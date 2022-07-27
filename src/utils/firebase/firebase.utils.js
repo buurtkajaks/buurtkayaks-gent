45621d42588fde467c1cec55ebc5fa59f6051fbd
buurtkajaks <buurtkajaks@gmail.com>
2022-07-27 19:30:08 +0200

    eslint

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -26,7 +26,7 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-const firebaseApp = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
